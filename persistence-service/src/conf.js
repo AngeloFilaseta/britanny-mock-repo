08a9684b574ee5c212702f3d97aec8dc6109bb4d
Tale152 <alessandro.talmi@studio.unibo.it>
2021-11-25 21:48:43 +0100

    tolte righe inutili

--- persistence-service/src/conf.js
+++ persistence-service/src/conf.js
@@ -1,6 +1,4 @@
 module.exports = {
-    clientAddress: 'http://localhost',
-    clientPort: 5000,
     dbAddress: 'mongodb://database:27017/brittany_persistence',
     testDbAddress: 'mongodb://127.0.0.1/brittany_persistence_test',
     servicePort: 8081,
