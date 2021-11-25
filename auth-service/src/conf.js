3b2f8281e7372ea753108857da6013ab37f8153a
Tale152 <alessandro.talmi@studio.unibo.it>
2021-11-25 16:24:03 +0100

    toh, è tornato auth-service

---
+++ auth-service/src/conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+    clientAddress: 'http://localhost',
+    clientPort: 5000,
+    dbAddress: 'mongodb://database:27017/brittany_auth',
+    testDbAddress: 'mongodb://127.0.0.1/brittany_auth',
+    servicePort: 8080,
+    asciiArt: 
+        'TODO BRITTANY AUTH ASCII ART',
+}
