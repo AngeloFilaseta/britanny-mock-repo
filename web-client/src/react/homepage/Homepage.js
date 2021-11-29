c2f52bc6fbc8be9e28ef7fc79d6e2ce726c4a5be
Tale152 <alessandro.talmi@studio.unibo.it>
2021-11-29 21:34:05 +0100

    yf

--- web-client/src/react/homepage/Homepage.js
+++ web-client/src/react/homepage/Homepage.js
@@ -31,7 +31,7 @@ export default function Homepage() {
           console.log(result)
         })
       }}>Value exists</Button>
-      <p>{JSON.stringify(process.env)}</p>
+      <p>{persistenceService}</p>
     </>
   )
   
