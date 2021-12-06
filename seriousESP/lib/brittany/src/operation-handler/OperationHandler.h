038a9e1490967beee6f517247c9cc70ba435e2d2
Angelo Filaseta <angelo.filaseta@studio.unibo.it>
2021-12-06 15:09:43 +0100

    Added OperationHandler

---
+++ seriousESP/lib/brittany/src/operation-handler/OperationHandler.h
@@ -0,0 +1,24 @@
+#ifndef BRITTANY_OPERATION_HANDLER_H
+#define BRITTANY_OPERATION_HANDLER_H
+
+#include <string>
+#include <json/json.h>
+#include "OperationHandlerResult.h"
+
+class OperationHandler {
+
+public:
+
+    OperationHandler(std::string path);
+
+    std::string path();
+
+    OperationHandlerResult handle(Json::Value args); // =0
+
+private:
+
+    std::string _path;
+
+};
+
+#endif //BRITTANY_OPERATION_HANDLER_H
